4a520c3d0c01b74e70f207bb80769b128c48c222
Justin L Hudson <justinlhudson@gmail.com>
2015-03-31 17:23:43 -0400

    changed application name to app to be more generic

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'web-application-client',
+    modulePrefix: 'app',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
